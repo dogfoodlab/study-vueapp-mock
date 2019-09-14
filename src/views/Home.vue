7a76f9fe8a5970967570eeed0f67f93e4d49fc4f
Tatsuya Kitajima <dogfoodinglab@gmail.com>
2019-09-14 23:27:54 +0900

    Fix menu.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,8 +5,36 @@
       <h1>Home</h1>
 
       <ul>
-        <li>
-          <router-link :to="{name:'layouts'}">Layouts</router-link>
+        <li>Layouts
+          <ul>
+            <li>
+              <router-link :to="{name:'layouts/baseline'}">Baseline</router-link>
+            </li>
+            <li>
+              <router-link :to="{name:'layouts/baseline-flipped'}">Baseline Flipped</router-link>
+            </li>
+            <li>
+              <router-link :to="{name:'layouts/centered'}">Centered</router-link>
+            </li>
+            <li>
+              <router-link :to="{name:'layouts/complex'}">Complex</router-link>
+            </li>
+            <li>
+              <router-link :to="{name:'layouts/dark'}">Dark</router-link>
+            </li>
+            <li>
+              <router-link :to="{name:'layouts/google-contacts'}">Google Contacts</router-link>
+            </li>
+            <li>
+              <router-link :to="{name:'layouts/google-keep'}">Google Keep</router-link>
+            </li>
+            <li>
+              <router-link :to="{name:'layouts/google-youtube'}">Google Youtube</router-link>
+            </li>
+            <li>
+              <router-link :to="{name:'layouts/sandbox'}">Sandbox</router-link>
+            </li>
+          </ul>
         </li>
         <li>
           <router-link :to="{name:'vuetify'}">Vuetify</router-link>
--- src/views/layouts/Home.vue
+++ src/views/layouts/Home.vue
@@ -3,37 +3,6 @@
     <v-container>
 
       <h1>Pre-made layouts</h1>
-      <ul>
-        <li>
-          <router-link :to="{name:'layouts/baseline'}">Baseline</router-link>
-        </li>
-        <li>
-          <router-link :to="{name:'layouts/baseline-flipped'}">Baseline Flipped</router-link>
-        </li>
-        <li>
-          <router-link :to="{name:'layouts/centered'}">Centered</router-link>
-        </li>
-        <li>
-          <router-link :to="{name:'layouts/complex'}">Complex</router-link>
-        </li>
-        <li>
-          <router-link :to="{name:'layouts/dark'}">Dark</router-link>
-        </li>
-        <li>
-          <router-link :to="{name:'layouts/google-contacts'}">Google Contacts</router-link>
-        </li>
-        <li>
-          <router-link :to="{name:'layouts/google-keep'}">Google Keep</router-link>
-        </li>
-        <li>
-          <router-link :to="{name:'layouts/google-youtube'}">Google Youtube</router-link>
-        </li>
-        <li>
-          <router-link :to="{name:'layouts/sandbox'}">Sandbox</router-link>
-        </li>
-      </ul>
-
-      <router-view />
 
     </v-container>
   </v-content>
