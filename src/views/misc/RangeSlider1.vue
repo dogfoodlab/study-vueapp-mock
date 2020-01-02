324e5085cdb7899ac9a88954aac6dc24871cb71d
Tatsuya Kitajima <dogfoodinglab@gmail.com>
2020-01-02 16:36:55 +0900

    Fix.

--- src/views/misc/RangeSlider1.vue
+++ src/views/misc/RangeSlider1.vue
@@ -1,5 +1,8 @@
 <template>
-  <v-container>
+  <v-container
+    pl-12
+    pr-12
+  >
     <range-slider
       skin="flat"
       type="double"
@@ -9,7 +12,7 @@
       :max="slider.max"
       :from="slider.from"
       :to="slider.to"
-      :prettify="val => formatDate(val)"
+      :prettify="valueOfDate => sliderDateFormat(valueOfDate)"
       @change="onChangeRangeSlider"
       @update="onUpdateRangeSlider"
     />
@@ -24,26 +27,24 @@ export default {
   components: {
     'range-slider': RangeSlider
   },
-  mounted () {
-  },
   methods: {
-    onChangeRangeSlider (data) {
-      // console.log(data)
+    sliderDateFormat (valueOfDate) {
+      return moment(valueOfDate).format('YYYY-MM-DD HH:mm:ss')
     },
-    onUpdateRangeSlider (data) {
-      // console.log(new Date(value))
+    onChangeRangeSlider (slider) {
+      // console.log(slider)
     },
-    formatDate (value) {
-      return moment(new Date(value)).format('YYYY-MM-DD HH:mm:ss')
+    onUpdateRangeSlider (slider) {
+      // console.log(slider)
     }
   },
   data () {
     return {
       slider: {
-        min: moment('2020-1-1', 'YYYY-MM-DD').toDate().valueOf(),
-        max: moment('2020-3-31', 'YYYY-MM-DD').toDate().valueOf(),
-        from: moment('2020-2-1', 'YYYY-MM-DD').toDate().valueOf(),
-        to: moment('2020-3-1', 'YYYY-MM-DD').toDate().valueOf()
+        min: moment('2020-04-01', 'YYYY-MM-DD').valueOf(),
+        max: moment('2021-03-31', 'YYYY-MM-DD').valueOf(),
+        from: moment('2020-07-01', 'YYYY-MM-DD').valueOf(),
+        to: moment('2020-12-31', 'YYYY-MM-DD').valueOf()
       }
     }
   }
